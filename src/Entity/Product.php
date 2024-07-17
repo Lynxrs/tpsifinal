010d1cbb87dc799b843ac997252aba1e42b0b291
Lynx <101434584+Lynxrs@users.noreply.github.com>
2024-07-17 06:44:09 +0200

    initial commit

--- src/Entity/Product.php
+++ src/Entity/Product.php
@@ -11,10 +11,40 @@ class Product
     #[ORM\Id]
     #[ORM\GeneratedValue]
     #[ORM\Column]
-    private ?int $id = null;
+    public ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    public ?string $name = null;
+
+    #[ORM\Column]
+    public ?float $price = null;
 
     public function getId(): ?int
     {
         return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): static
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getPrice(): ?float
+    {
+        return $this->price;
+    }
+
+    public function setPrice(float $price): static
+    {
+        $this->price = $price;
+
+        return $this;
     }
 }
